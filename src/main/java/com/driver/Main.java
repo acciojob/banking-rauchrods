b7dd06518808f994aaf6704e299e2777d5af70b6
rauch <rauchrodrigues@gmail.com>
2022-12-30 18:07:35 +0530

    jk2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,14 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
           BankAccount acc1 = new BankAccount("rauch",10000,500);
-        System.out.println(acc1.generateAccountNumber(3,22));    ;
+        System.out.println(acc1.generateAccountNumber(3,22));
+
+        SavingsAccount  s1 = new SavingsAccount("lala",1000,500,7);
+        s1.withdraw(500);
+        System.out.println(s1.getSimpleInterest(5));
+        System.out.println(s1.getCompoundInterest(1,5));;
+
+        CurrentAccount c1 = new CurrentAccount("bambu",10000,"ABCDDD");
+        c1.validateLicenseId();
     }
 }
